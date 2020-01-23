60949fbf8c952587222f334d5504df96f68bea14
Allie Robinson <allierob93@gmail.com>
2020-01-23 14:49:06 -0600

    MVP met

--- queries.sql
+++ queries.sql
@@ -1,12 +1,15 @@
 -- Multi-Table Query Practice
 
--- Display the ProductName and CategoryName for all products in the database. Shows 77 records.
+-- Display the ProductName and CategoryName 
+-- for all products in the database. 
+-- Shows 77 records.
 
 SELECT p.productname, c.categoryname
 FROM product AS p
 JOIN category AS c ON p.CategoryId = c.Id;
 
--- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+-- Display the order Id and shipper CompanyName 
+-- for all orders placed before August 9 2012. Shows 429 records.
 
 SELECT o.id, s.CompanyName
 FROM [order] AS o
@@ -21,9 +24,12 @@ JOIN orderdetail AS o ON o.productId = p.id
 WHERE orderId = 10251
 ORDER BY p.ProductName;
 
--- Display the OrderID, Customer's Company Name and the employee's LastName for every order. All columns should be labeled clearly. Displays 16,789 records.
+-- Display the OrderID, Customer's Company Name 
+-- and the employee's LastName 
+-- for every order. All columns should be labeled
+-- clearly. Displays 16,789 records.
 
-SELECT od.OrderId
+SELECT distinct od.OrderId
 , c.CompanyName AS CustomerCompany
 , e.LastName AS EmployeeLastName
 FROM OrderDetail as od
